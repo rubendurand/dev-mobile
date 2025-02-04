1b662fd6da116af3b49901d055d138ad06ba8b66
Ruben Durand <ruben.durand@etu.unilim.fr>
2025-02-04 11:43:32 +0100

    Création des boutons et champs de texte, navigation entre les pages

--- TP3NavigationCompose/app/src/main/java/com/example/tp3navigationcompose/MainActivity.kt
+++ TP3NavigationCompose/app/src/main/java/com/example/tp3navigationcompose/MainActivity.kt
@@ -4,13 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+import androidx.navigation.compose.rememberNavController
 import com.example.tp3navigationcompose.ui.theme.TP3NavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +41,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TP3NavigationComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MaterialTheme{
+                    AppNavigation()
                 }
             }
         }
@@ -45,3 +64,57 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home"){
+            HomeScreen(navController = navController)
+        }
+        composable("form"){
+            FormScreen(navController = navController)
+        }
+    }
+
+}
+
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Bienvenue dans ma première applicaiton compose navigation",
+            style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {navController.navigate("form")}) {
+            Text(text = "Accéder au formulaire")
+        }
+    }
+}
+
+@Composable
+fun FormScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Page du formulaire", style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(
+            onClick = { navController.popBackStack() }) {
+            Text(text = "Retour")
+        }
+    }
+}
+
+
